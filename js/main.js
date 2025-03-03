e5be7f88e1c29f846b4eee614217f051c68fd311
Francesco <you@example.com>
2025-03-03 17:40:41 +0100

    Add promise.all

--- js/main.js
+++ js/main.js
@@ -6,7 +6,34 @@
     Stampare i dati in console in un messaggio ben formattato.
     Testa la funzione con la query "london" */
 
-const getDashboardData = async (query) => {};
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = response.json();
+  return obj;
+}
+
+const getDashboardData = async (query) => {
+  const destination = fetchJson(
+    `https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`
+  );
+
+  const weather = fetchJson(
+    `https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`
+  );
+  const airport = fetchJson(
+    `https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`
+  );
+
+  const promises = [destination, weather, airport];
+  const [destinations, weathers, airports] = await Promise.all(promises);
+  return {
+    city: destinations[0].name,
+    country: destinations[0].country,
+    temperature: weathers[0].temperature,
+    weather: weathers[0].weather_description,
+    airport: airports[0].name,
+  };
+};
 
 getDashboardData("london")
   .then((data) => {
